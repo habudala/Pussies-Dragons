0df9d8191aad8fde80f26d5706c1885e16a31717
habudala <habudala@live.com>
2016-03-22 22:46:34 -0400

    Adding some more JQ

--- js/script.js
+++ js/script.js
@@ -1,38 +1,34 @@
-function runAway () {
+$("#run").click(function () {
 
 	document.getElementById("message").innerHTML = "You ran away, like a little pussy!";
 
 	document.getElementById("choice").src = "dice-img/fleeing.png";
 
-
-
 	//don't forget if you are using classes, or IDs. It gave you trouble this time. Also getElementByClassName 
 	// said "it's not a function"
 
-
 	//to change an image, use the src property!!!
+});
+	
+$("#fight").click(function (){
+	document.getElementById("choice").src = "dice-img/dragon.png";
+});
+	
 
-}
-
-	$("#fight").click(function (){
+$("#fight").click(function (){
 	$("#img1").addClass("dieOne");
 	$("#img2").addClass("dieTwo");
-
+	setTimeout(fight,2000);
 });
 
-	$("#fight").mouseenter(function (){
+$("#fight").mouseenter(function (){
 	$("#img1").removeClass("dieOne");
 	$("#img2").removeClass("dieTwo");
-
 });
 
 	
 
 function fight () {
-
-
-
-
 
 	var die1 = Math.floor(Math.random()*6)+1;// getting a random number for die 1
 	// alert(die1);--> checking if it works
@@ -43,17 +39,13 @@ function fight () {
 	// alert(die1);--> checking if it works
 	// depending on the number ,that's which image is going to pop up
 	document.getElementById("img2").src = "dice-img/d" + die2 + ".gif";
+	
 	// this if-else statement decides based on your rolls whether or not you won
 	if (die1 + die2 >= 8) { document.getElementById("message"). innerHTML = "Congratulations! You are now officially a dragon slayer!";
-
-
 	}else{document.getElementById("message").innerHTML = "I'm sorry, but you got burned to a crisp! Better luck next time!";}
 	//this resets the picture of the dragon after you hit fight (in case you hit run beforehand)
-document.getElementById("choice").src = "dice-img/dragon.png";
-
-
+	
 }
-
 
 
 
